1186e7fd9f2a47d21ab89f384865f6558a884252
Janhavi Mudaliar <janhavimudaliar@gmail.com>
2023-09-23 16:53:27 +0530

    all files

---
+++ email-service/src/main/java/com/controller/MessageController.java
@@ -0,0 +1,43 @@
+package com.controller;
+
+import com.model.Ordermail;
+import com.service.MessageService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+@CrossOrigin(origins="*")
+@RestController
+@RequestMapping("/email")
+public class MessageController {
+    @Autowired
+    private MessageService messageService;
+    public static final Logger LOGGER=LoggerFactory.getLogger(MessageController.class);
+    @GetMapping("/messages")
+    public List<Ordermail> getUnreadMessages(@RequestHeader("User") String email) {
+    	System.out.println(email);
+    	LOGGER.info("Order place......notification is been sent to user"+email);
+        return messageService.getUnreadMessagesByEmail(email);
+    }
+    
+    @PatchMapping("/update")
+		public ResponseEntity<?>updateStatus(@RequestBody Ordermail ordermail)
+		{
+			messageService.updatemessage(ordermail);
+			LOGGER.info("The user has viewed the notification(seen)");
+			return new ResponseEntity("Message updated successfully",HttpStatus.ACCEPTED);
+		
+		}
+}
---
+++ email-service/src/main/java/com/service/MessageService.java
@@ -0,0 +1,26 @@
+package com.service;
+
+import com.model.Ordermail;
+import com.repository.OrdermailRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@Service
+public class MessageService {
+    @Autowired
+    private OrdermailRepository orderMailRepository;
+
+    public List<Ordermail> getUnreadMessagesByEmail(String email) {
+        return orderMailRepository.findByEmail(email);
+    }
+    public void updatemessage(Ordermail ordermail)
+   	{
+   		orderMailRepository.save(ordermail);
+   	}
+}
